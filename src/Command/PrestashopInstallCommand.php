4fc3103221bdd7f31989bd4ae60b38c64eaa4137
Matthieu Mota <matthieumota@gmail.com>
2017-12-15 17:41:32 +0100

    Refactor code, check prestashop 1.7 zip, set application name & version

--- src/Command/PrestashopInstallCommand.php
+++ src/Command/PrestashopInstallCommand.php
@@ -17,52 +17,82 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class PrestashopInstallCommand extends Command
 {
-    /**
-     * @var ProgressBar
-     */
-    private $progressBar;
-
-    /**
-     * @var OutputInterface
-     */
-    private $output;
-
     protected function configure()
     {
         $this->setName('prestashop:install')
             ->setDescription('Install Prestashop.')
+            ->addOption('psVersion', null, InputOption::VALUE_OPTIONAL, 'Prestashop version')
             ->setHelp('This command can install Prestashop.');
     }
 
     public function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->output = $output;
+        $helper = $this->getHelper('question');
+
+        $psVersion = $input->getOption('psVersion');
+
+        if (null === $psVersion) {
+            $askVersion = new ChoiceQuestion('What version do you want to install ? [1.7.2.4]', [
+                '1.6.1.17',
+                '1.7.2.4'
+            ], '1.7.2.4');
+            $psVersion = $helper->ask($input, $output, $askVersion);
+        }
 
         if (!file_exists(Application::ROOT_DIR.'prestashop.zip')) {
             $output->writeln("<info>Downloading Prestashop...</info>");
 
             $client = new Client();
+            $progressBar = null;
             $client->request(
                 'GET',
-                'https://www.prestashop.com/download/old/prestashop_1.7.2.4.zip',
+                'https://www.prestashop.com/download/old/prestashop_'.$psVersion.'.zip',
                 [
-                    'progress' => function ($downloadSize, $downloaded) {
-                        $this->progress($downloadSize, $downloaded);
+                    'progress' => function ($downloadSize, $downloaded) use (&$output, &$progressBar) {
+                        if ($downloadSize < 1 * 1024 * 1024) {
+                            return;
+                        }
+
+                        if (null === $progressBar) {
+                            ProgressBar::setPlaceholderFormatterDefinition('max', function (ProgressBar $bar) {
+                                return Helper::formatMemory($bar->getMaxSteps());
+                            });
+
+                            ProgressBar::setPlaceholderFormatterDefinition('current', function (ProgressBar $bar) {
+                                return Helper::formatMemory($bar->getProgress());
+                            });
+
+                            $progressBar = new ProgressBar($output, $downloadSize);
+                            $progressBar->setFormat('%current%/%max% %bar%  %percent:3s%%');
+
+                            // $progressBar->setProgressCharacter("\xF0\x9F\x8D\xBA");
+                            $progressBar->setEmptyBarCharacter('░');
+                            $progressBar->setProgressCharacter('');
+                            $progressBar->setBarCharacter('▓');
+
+                            $progressBar->start();
+                        }
+
+                        $progressBar->setProgress($downloaded);
                     },
                     'sink' => Application::ROOT_DIR.'prestashop.zip'
                 ]
             );
 
-            if (null !== $this->progressBar) {
-                $this->progressBar->finish();
+            if (null !== $progressBar) {
+                $progressBar->finish();
             }
 
             $output->writeln("\n");
         }
+
+        $output->writeln("<info>Extracting Prestashop...</info>");
 
         $zip = new \ZipArchive();
 
@@ -73,43 +103,15 @@ class PrestashopInstallCommand extends Command
 
         $zip->extractTo(Application::ROOT_DIR.'prestashop');
 
-        if (true !== $zip->open(Application::ROOT_DIR.'prestashop/prestashop.zip')) {
-            $output->writeln("<error>Unable to unzip ".Application::ROOT_DIR."prestashop.zip</error>");
-            return;
-        }
+        if (version_compare($psVersion, '1.7', '>=')) {
+            if (true !== $zip->open(Application::ROOT_DIR.'prestashop/prestashop.zip')) {
+                $output->writeln("<error>Unable to unzip ".Application::ROOT_DIR."prestashop.zip</error>");
+                return;
+            }
 
-        $output->writeln("<info>Extracting Prestashop...</info>");
-        $zip->extractTo(Application::ROOT_DIR.'prestashop');
+            $zip->extractTo(Application::ROOT_DIR.'prestashop');
+        }
 
         $output->writeln("<info>Prestashop is installed !</info>");
-    }
-
-    private function progress($downloadSize, $downloaded)
-    {
-        if ($downloadSize < 1 * 1024 * 1024) {
-            return;
-        }
-
-        if (null === $this->progressBar) {
-            ProgressBar::setPlaceholderFormatterDefinition('max', function (ProgressBar $bar) {
-                return Helper::formatMemory($bar->getMaxSteps());
-            });
-
-            ProgressBar::setPlaceholderFormatterDefinition('current', function (ProgressBar $bar) {
-                return Helper::formatMemory($bar->getProgress());
-            });
-
-            $this->progressBar = new ProgressBar($this->output, $downloadSize);
-            $this->progressBar->setFormat('%current%/%max% %bar%  %percent:3s%%');
-
-            // $this->progressBar->setProgressCharacter("\xF0\x9F\x8D\xBA");
-            $this->progressBar->setEmptyBarCharacter('░');
-            $this->progressBar->setProgressCharacter('');
-            $this->progressBar->setBarCharacter('▓');
-
-            $this->progressBar->start();
-        }
-
-        $this->progressBar->setProgress($downloaded);
     }
 }
