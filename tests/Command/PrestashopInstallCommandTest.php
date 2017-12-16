c805659b7205e5575f56e8cd1dfa52f76a26cccd
Matthieu Mota <matthieumota@gmail.com>
2017-12-16 22:23:18 +0100

    Minimal test phpunit

---
+++ tests/Command/PrestashopInstallCommandTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the PrestashopConsole package.
+ *
+ * (c) Matthieu Mota <matthieu@boxydev.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Boxydev\Command;
+
+use Boxydev\Prestashop\Application;
+use Boxydev\Prestashop\Command\PrestashopInstallCommand;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class PrestashopInstallCommandTest extends TestCase
+{
+    public function testInstallPrestashop17()
+    {
+        $application = new Application();
+        $application->add(new PrestashopInstallCommand());
+
+        $command = $application->find('prestashop:install');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array(
+            'command'  => $command->getName(),
+            '--psVersion' => '1.7.2.4'
+        ));
+
+        $output = $commandTester->getDisplay();
+        $this->assertContains('Prestashop 1.7.2.4 is now installed', $output);
+    }
+}
